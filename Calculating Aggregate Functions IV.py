90ecd1046b370877366f4c00251e50c8c0c950d8
João <40552750+Joao-Regis@users.noreply.github.com>
2019-10-03 10:37:16 -0700

    Create Calculating Aggregate Functions IV.py

---
+++ Calculating Aggregate Functions IV.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+orders = pd.read_csv('orders.csv')
+print(orders)
+
+shoe_counts = orders.groupby(['shoe_type', 'shoe_color'])['id'].count().reset_index()
+
+print(shoe_counts)
